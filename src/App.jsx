6bc2315eeaebd60073954fdda6d70ab662aec7dd
MEENALOSINI.V <125140347+meenavetri@users.noreply.github.com>
2024-03-20 13:15:13 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import RandomColor from './components/randomcolorgenerator'
+import Accordian from './components/accordian'
+import Rating from './components/rating'
+import ImageSlider from './components/image-slider'
+import LoadMoreData from './components/Load-more-products'
+import TreeView from './components/tree-view'
+import menus from './components/tree-view/data'
+
+function App() {
+  return (
+    <>
+      {/* <Accordian/> */}
+      {/* <RandomColor/> */}
+      {/* <Rating /> */}
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} 
+      page={'1'} limit={'10'}/> */}
+      {/* <LoadMoreData/> */}
+      <TreeView menus={menus}/>
+    </>
+  )
+}
+
+export default App
