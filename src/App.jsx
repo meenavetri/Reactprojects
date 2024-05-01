c368415f78aad45a0c66ca9a0a474782e151a7a2
MEENALOSINI.V <125140347+meenavetri@users.noreply.github.com>
2024-05-01 12:47:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-
-import RandomColor from './components/randomcolorgenerator'
+import menus from './components/tree-view/data'
 import Accordian from './components/accordian'
-import Rating from './components/rating'
 import ImageSlider from './components/image-slider'
 import LoadMoreData from './components/Load-more-products'
+import QRCodeGenertator from './components/QRCodeGenerator'
+import RandomColor from './components/randomcolorgenerator'
 import TreeView from './components/tree-view'
-import menus from './components/tree-view/data'
+import Rating from './components/rating'
+import LightDarkMode from './components/ThemeChange'
 
 function App() {
   return (
     <>
-      <Accordian/>
-      <RandomColor/>
-      <Rating />
-      <ImageSlider url={"https://picsum.photos/v2/list"} 
-      page={'1'} limit={'10'}/>
-      <LoadMoreData/>
-      <TreeView menus={menus}/>
-      <Rating/>
+     <Accordian/>
+    <ImageSlider
+    url={"https://picsum.photos/v2/list"}
+    page={"1"}
+    limit={"10"}
+    />
+    <LoadMoreData/>
+    <QRCodeGenertator/>
+    <RandomColor/>
+    <TreeView menus={menus}/>
+    <Rating/>
     <LightDarkMode/>
+    </>
     </>
   )
 }
